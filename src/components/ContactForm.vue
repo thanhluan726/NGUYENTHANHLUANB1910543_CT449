1478a586e3b5d8963764d21dfe191037288b80e4
thanhluan726 <113674240+thanhluan726@users.noreply.github.com>
2022-11-04 14:44:17 +0700

    Chỉnh sửa và đẩy lại toàn bộ code

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -13,9 +13,18 @@
     />
     <ErrorMessage name="name" class="error-feedback" />
     </div>
-  
     <div class="form-group">
-    <label for="address">Date</label>
+    <label for="email">E-mail</label>
+    <Field
+    name="email"
+    type="email"
+    class="form-control"
+    v-model="contactLocal.email"
+    />
+    <ErrorMessage name="email" class="error-feedback" />
+    </div>
+    <div class="form-group">
+    <label for="address">Địa chỉ</label>
     <Field
     name="address"
     type="text"
@@ -25,7 +34,7 @@
     <ErrorMessage name="address" class="error-feedback" />
     </div>
     <div class="form-group">
-    <label for="phone">Content</label>
+    <label for="phone">Điện thoại</label>
     <Field
     name="phone"
     type="tel"
@@ -34,7 +43,18 @@
     />
     <ErrorMessage name="phone" class="error-feedback" />
     </div>
-
+    <div class="form-group form-check">
+    <input
+    name="favorite"
+    type="checkbox"
+    class="form-check-input"
+    v-model="contactLocal.favorite"
+    />
+    <label for="favorite" class="form-check-label">
+    <strong>Liên hệ yêu thích</strong>
+</label>
+</div>
+<div class="form-group">
 <button class="btn btn-primary">Lưu</button>
 <button
 v-if="contactLocal._id"
@@ -44,6 +64,7 @@ class="ml-2 btn btn-danger"
 >
 Xóa
 </button>
+</div>
 </Form>
 </template>
 <script>
@@ -66,7 +87,17 @@ name: yup
 .required("Tên phải có giá trị.")
 .min(2, "Tên phải ít nhất 2 ký tự.")
 .max(50, "Tên có nhiều nhất 50 ký tự."),
-
+email: yup
+.string()
+.email("E-mail không đúng.")
+.max(50, "E-mail tối đa 50 ký tự."),
+address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+phone: yup
+.string()
+.matches(
+/((09|03|07|08|05)+([0-9]{8})\b)/g,
+"Số điện thoại không hợp lệ."
+),
 });
 return {
 // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
